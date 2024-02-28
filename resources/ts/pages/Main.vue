09983b63df60429b4ced11f4032f68aa8c36dac9
Grégory Barile <gregory@jarod-xp.dev>
2024-02-28 06:40:19 +0100

    feat: implement main layout and first routes

---
+++ resources/ts/pages/Main.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <p>FORMATION VUE</p>
+    <nav>
+      <!--  WITH LINK: NOT RECOMMENDED
+       <router-link to="/home">HOME</router-link>
+      <router-link to="/tasks">TASK LIST</router-link>
+     -->
+      <router-link :to="{ name: 'home' }">HOME</router-link>
+      <router-link :to="{ name: 'tasks' }">TASK LIST</router-link>
+    </nav>
+  </header>
+
+  <router-view />
+</template>
+
+<style scoped>
+header {
+  background-color: #f0f0f0;
+  padding: 10px;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+}
+
+header > p {
+  margin-right: 10rem;
+}
+
+header > nav {
+  display: flex;
+  gap: 4rem;
+}
+</style>
--- resources/ts/router/router.ts
+++ resources/ts/router/router.ts
@@ -2,9 +2,31 @@ import {
     type RouteRecordRaw,
     createRouter,
     createWebHistory,
-} from "vue-router";
+} from 'vue-router';
+import Home from '../features/home/Home.vue';
+import Tasks from '../features/tasks/Tasks.vue';
+import Main from '../pages/Main.vue';
 
-const routes: RouteRecordRaw[] = [];
+const routes: RouteRecordRaw[] = [
+    {
+        path: '/',
+        name: 'main',
+        component: Main,
+        redirect: { name: 'home' },
+        children: [
+            {
+                path: '/home',
+                name: 'home',
+                component: Home,
+            },
+            {
+                path: '/tasks',
+                name: 'tasks',
+                component: Tasks,
+            },
+        ],
+    },
+];
 
 export default createRouter({
     history: createWebHistory(),
